1d3e65d35bac0b651b01522ff22f184cce64dc4b
kubh1441 <kubh1441@gmail.com>
2022-09-17 23:11:36 +0900

    カリキュラム08-3課題4

--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -8,13 +8,14 @@
     </head>
     <body>
         <h1>Blog Name</h1>
-        @foreach($posts as $post)
         <div class='posts'>
+            @foreach($posts as $post)
             <div class='post'>
                 <h2 class='title'>{{$post->title}}</h2>
                 <p class='body'>{{$post->body}}</p>
             </div>
+            @endforeach
         </div>
-        @endforeach
+        <div class='paginate'>{{ $posts->links()}}</div>
     </body>
 </html>
